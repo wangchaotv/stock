daf38b67795cb8977c07ebcdfdcfc4f4b83032c9
wangchao <15715703070@163.com>
2025-05-06 21:58:22 +0800

    1

--- src/Right/Balancesheet.js
+++ src/Right/Balancesheet.js
@@ -63,23 +63,45 @@ const Balancesheet = (props) => {
           <tr>
             <td colSpan={showCount + 2}>&nbsp;</td>
           </tr>
+          <TR
+            name="股东权益合计"
+            data={data}
+            valueKey="total_hldr_eqy_inc_min_int"
+          />
           <TR name="负债合计" data={data} valueKey="total_liab" />
           <TR name="流动负债合计" data={data} valueKey="total_cur_liab" />
           <TR name="非流动负债合计" data={data} valueKey="total_ncl" />
           <tr>
             <td colSpan={showCount + 2}>&nbsp;</td>
           </tr>
+          <tr>
+            <td colSpan={showCount + 2} className="td-title">
+              流动资产
+            </td>
+          </tr>
+          <TR name="货币资金" data={data} valueKey="money_cap" />
+          <TR name="应收票据" data={data} valueKey="notes_receiv" />
+          <TR name="应收账款" data={data} valueKey="accounts_receiv" />
+          <TR name="应收款项融资" data={data} valueKey="receiv_financing" />
+          <TR name="预付款项" data={data} valueKey="prepayment" />
+          <TR name="应收利息" data={data} valueKey="int_receiv" />
+          <TR name="应收股利" data={data} valueKey="div_receiv" />
+          <TR name="其他应收款" data={data} valueKey="oth_receiv" />
+          <TR name="存货" data={data} valueKey="inventories" />
+          <TR name="持有待售的资产" data={data} valueKey="hfs_assets" />
           <TR
-            name="股东权益合计"
+            name="一年内到期的非流动资产"
             data={data}
-            valueKey="total_hldr_eqy_inc_min_int"
+            valueKey="nca_within_1y"
           />
+          <TR name="其他流动资产" data={data} valueKey="oth_cur_assets" />
           <tr>
             <td colSpan={showCount + 2}>&nbsp;</td>
           </tr>
-          <TR name="货币资金" data={data} valueKey="money_cap" />
           <tr>
-            <td colSpan={showCount + 2}>&nbsp;</td>
+            <td colSpan={showCount + 2} className="td-title">
+              固定资产
+            </td>
           </tr>
           <TR name="固定资产" data={data} valueKey="fix_assets" />
         </tbody>
