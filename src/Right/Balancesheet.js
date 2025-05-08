8627b3d7fbd2edb4a55da5baf7e1ee628df10c3c
wangchao <15715703070@163.com>
2025-05-08 21:24:24 +0800

    1

--- src/Right/Balancesheet.js
+++ src/Right/Balancesheet.js
@@ -80,14 +80,14 @@ const Balancesheet = (props) => {
             </td>
           </tr>
           <TR name="货币资金" data={data} valueKey="money_cap" />
+          <TR name="交易性金融资产" data={data} valueKey="trad_asset" />
           <TR name="应收票据" data={data} valueKey="notes_receiv" />
           <TR name="应收账款" data={data} valueKey="accounts_receiv" />
           <TR name="应收款项融资" data={data} valueKey="receiv_financing" />
           <TR name="预付款项" data={data} valueKey="prepayment" />
-          <TR name="应收利息" data={data} valueKey="int_receiv" />
-          <TR name="应收股利" data={data} valueKey="div_receiv" />
-          <TR name="其他应收款" data={data} valueKey="oth_receiv" />
+          <TR name="其他应收款(合计)" data={data} valueKey="oth_rcv_total" />
           <TR name="存货" data={data} valueKey="inventories" />
+          <TR name="合同资产" data={data} valueKey="contract_assets" />
           <TR name="持有待售的资产" data={data} valueKey="hfs_assets" />
           <TR
             name="一年内到期的非流动资产"
@@ -100,10 +100,26 @@ const Balancesheet = (props) => {
           </tr>
           <tr>
             <td colSpan={showCount + 2} className="td-title">
-              固定资产
+              非流动资产
             </td>
           </tr>
+          <TR name="其他债权投资" data={data} valueKey="oth_debt_invest" />
+          <TR name="长期应收款" data={data} valueKey="lt_rec" />
+          <TR name="长期股权投资" data={data} valueKey="lt_eqt_invest" />
+          <TR
+            name="其他非流动金融资产"
+            data={data}
+            valueKey="oth_illiq_fin_assets"
+          />
+          <TR name="投资性房地产" data={data} valueKey="invest_real_estate" />
           <TR name="固定资产" data={data} valueKey="fix_assets" />
+          <TR name="在建工程" data={data} valueKey="cip" />
+          <TR name="使用权资产" data={data} valueKey="use_right_assets" />
+          <TR name="无形资产" data={data} valueKey="intan_assets" />
+          <TR name="商誉" data={data} valueKey="goodwill" />
+          <TR name="长期待摊费用" data={data} valueKey="lt_amor_exp" />
+          <TR name="递延所得税资产" data={data} valueKey="defer_tax_assets" />
+          <TR name="其他非流动资产" data={data} valueKey="oth_nca" />
         </tbody>
       </table>
     </div>
